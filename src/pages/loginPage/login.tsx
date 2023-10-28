d8afb01b31e1c557895d2f5fc0470273979d631e
Rafael Nonato <rafaelnonato79@gmail.com>
2023-10-28 14:31:15 -0300

    corrigido bug do body

--- src/pages/loginPage/login.tsx
+++ src/pages/loginPage/login.tsx
@@ -7,15 +7,18 @@ import LoginComponent from '../../componentes/loginComponent';
 
 
 function LoginPage(){
-    return(<div className="main">
-    <div className="left-login ">
-        <img src={homeRH} alt="imagem de rh" width="100%" height="100%" />
-        <div className="overlay"></div>
-    </div>
+    return(
+        <body>
+     <div className="main">
+         <div className="left-login ">
+            <img src={homeRH} alt="imagem de rh" width="100%" height="100%" />
+            <div className="overlay"></div>
+        </div>
     <div className="right-login">
         <LoginComponent />
     </div>
-</div>
+</div>      
+        </body>
     )
 }
 
