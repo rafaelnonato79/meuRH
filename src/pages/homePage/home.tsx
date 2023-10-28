06e36b9e060c79885c65aef02869760fdcb1073a
Rafael Nonato <rafaelnonato79@gmail.com>
2023-10-28 14:23:46 -0300

    feito a divisão das divs dentro de main na homePage

--- src/pages/homePage/home.tsx
+++ src/pages/homePage/home.tsx
@@ -10,6 +10,24 @@ function HomePage(){
              <div className="nav" >
                  <Nav />
             </div>
+            <div className="main">
+                <div className="col-left">
+                    <div className="ponto">
+                        <h1>BATER PONTO</h1>
+                    </div>
+                    <div className="calendar">
+                        <h1>CALENDÁRIO</h1>
+                    </div>
+                </div>
+                <div className="col-right">
+                    <div className="timer">
+                        <h1>TIMER</h1>
+                    </div>
+                    <div className="noticias">
+                        <h1>NOTICIAS</h1>
+                    </div>
+                </div>
+            </div>
         </body>
     )
 }
